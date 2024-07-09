a40477ed9d3ea294aaffcc0b66ce03819787436d
Kush Patel <kush@kush.in>
2024-07-10 00:33:46 +0530

    added build and deploy scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+GZIP="/usr/bin/gzip -n"
+BROTLI="/usr/bin/brotli"
+rm -rf public/*
+hugo --minify
+
+cd public
+for text_file in $(find . \( -name "*.html" -o -name "*.xml" -o -name "*.css" -o -name "*.js" -o -name "*.json" -o -name "*.svg" -o -name "*.woff" -o -name "*.woff2" -o -name "*.ttf" -o -name "*.otf" -o -name "*.ico" \));
+    do
+        #echo "Compressing $text_file"
+        $BROTLI --best -k $text_file
+    done
+    exit 0
+rm sitemap.xml.br
