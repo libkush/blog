bffb174d37234c0dedb307b4cc0f7ab1b8d56644
Kush Patel <kush@localhost.localdomain>
2024-10-22 23:33:21 +0530

    changed deploy script for new server location

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 REMOTE_USER=ubuntu
-REMOTE_HOST=kush.in
-DIR=devbox-store/kush.in/html   # the directory where your website files should go
+REMOTE_HOST=devbox2
+DIR=/var/data/volumes/kush.in/html   # the directory where your website files should go
 
-./build.sh && rsync -avz --delete public/ ${REMOTE_USER}@${REMOTE_HOST}:~/${DIR} # this will delete everything on the server that's not in the local public folder 
+./build.sh && rsync -avz --delete public/ ${REMOTE_USER}@${REMOTE_HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
 
 exit 0
